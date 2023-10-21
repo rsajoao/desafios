df0d7ce52f9945877e151dd0ca8aa143fe1b0905
Joao Vitor <joaovitors@outlook.com>
2023-10-20 22:23:10 -0300

    feat: faz uma requisição e filtra os resultados

--- desafio-tmbd/main.js
+++ desafio-tmbd/main.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const key = process.env.MY_KEY;
-const url = 'https://api.themoviedb.org/3/authentication'
+const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-US&page=1&sort_by=popularity.asc'
 
 const headers = {
   Accept: 'application/json',
@@ -14,7 +14,12 @@ const config = { method: 'get', url, headers }
 const getMovies = async () => {
   try {
     const response = await axios(config);
-    console.log(response.status)
+
+    const movieList = response.data.results
+      .map(movie => movie.title)
+      .filter(movie => movie[0].toLowerCase() === 'a');
+  
+    console.log(movieList)
   } catch (error) {
     console.error('Erro!', error.message);
   } finally {
